10b4ba693fa0a12fbc610b8af27a4501b73a8c14
Rohan Verma <rsrohanverma@gmail.com>
2020-01-19 22:14:24 +0530

    camera

---
+++ app/src/main/java/com/example/cura/TinyDB.java
@@ -0,0 +1,251 @@
+package com.example.cura;
+
+/*
+ *  The "‚‗‚" character is not a comma, it is the SINGLE LOW-9 QUOTATION MARK unicode 201A
+ *  and unicode 2017 that are used for separating the items in a list.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+//import com.google.gson.Gson;
+
+
+public class TinyDB {
+
+    private static SharedPreferences preferences;
+    private String DEFAULT_APP_IMAGEDATA_DIRECTORY;
+    private String lastImagePath = "";
+    private Context context;
+
+    public TinyDB(Context appContext) {
+        preferences = PreferenceManager.getDefaultSharedPreferences(appContext);
+        this.context = appContext;
+    }
+
+
+    /**
+     * Decodes the Bitmap from 'path' and returns it
+     * @param path image path
+     * @return the Bitmap from 'path'
+     */
+    public Bitmap getImage(String path) {
+        Bitmap bitmapFromPath = null;
+        try {
+            bitmapFromPath = BitmapFactory.decodeFile(path);
+
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return bitmapFromPath;
+    }
+
+
+    /**
+     * Returns the String path of the last saved image
+     * @return string path of the last saved image
+     */
+    public String getSavedImagePath() {
+        return lastImagePath;
+    }
+
+
+    /**
+     * Saves 'theBitmap' into folder 'theFolder' with the name 'theImageName'
+     * @param theFolder the folder path dir you want to save it to e.g "DropBox/WorkImages"
+     * @param theImageName the name you want to assign to the image file e.g "MeAtLunch.png"
+     * @param theBitmap the image you want to save as a Bitmap
+     * @return returns the full path(file system address) of the saved image
+     */
+    public String putImage(String theFolder, String theImageName, Bitmap theBitmap) {
+        if (theFolder == null || theImageName == null || theBitmap == null)
+            return null;
+
+        this.DEFAULT_APP_IMAGEDATA_DIRECTORY = theFolder;
+        String mFullPath = setupFullPath(theImageName);
+
+        if (!mFullPath.equals("")) {
+            lastImagePath = mFullPath;
+            saveBitmap(mFullPath, theBitmap);
+        }
+
+        return mFullPath;
+    }
+
+
+    /**
+     * Saves 'theBitmap' into 'fullPath'
+     * @param fullPath full path of the image file e.g. "Images/MeAtLunch.png"
+     * @param theBitmap the image you want to save as a Bitmap
+     * @return true if image was saved, false otherwise
+     */
+    public boolean putImageWithFullPath(String fullPath, Bitmap theBitmap) {
+        return !(fullPath == null || theBitmap == null) && saveBitmap(fullPath, theBitmap);
+    }
+    public boolean putImageWithFullPath(String fullPath, Image theBitmap) {
+        return !(fullPath == null || theBitmap == null) && saveBitmap(fullPath, theBitmap);
+    }
+
+    /**
+     * Creates the path for the image with name 'imageName' in DEFAULT_APP.. directory
+     * @param imageName name of the image
+     * @return the full path of the image. If it failed to create directory, return empty string
+     */
+    private String setupFullPath(String imageName) {
+        File mFolder = new File(Environment.getExternalStorageDirectory(), DEFAULT_APP_IMAGEDATA_DIRECTORY);
+
+        if (isExternalStorageReadable() && isExternalStorageWritable() && !mFolder.exists()) {
+            if (!mFolder.mkdirs()) {
+                Log.e("ERROR", "Failed to setup folder");
+                return "";
+            }
+        }
+
+        return mFolder.getPath() + '/' + imageName;
+    }
+
+    /**
+     * Saves the Bitmap as a PNG file at path 'fullPath'
+     * @param fullPath path of the image file
+     * @param bitmap the image as a Bitmap
+     * @return true if it successfully saved, false otherwise
+     */
+    private boolean saveBitmap(String fullPath, Bitmap bitmap) {
+        if (fullPath == null || bitmap == null)
+            return false;
+
+        boolean fileCreated = false;
+        boolean bitmapCompressed = false;
+        boolean streamClosed = false;
+
+        File imageFile = new File(fullPath);
+
+        if (imageFile.exists())
+            if (!imageFile.delete())
+                return false;
+
+        try {
+            fileCreated = imageFile.createNewFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(imageFile);
+            bitmapCompressed = bitmap.compress(CompressFormat.PNG, 100, out);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            bitmapCompressed = false;
+
+        } finally {
+            if (out != null) {
+                try {
+                    out.flush();
+                    out.close();
+                    streamClosed = true;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    streamClosed = false;
+                }
+            }
+        }
+
+        return (fileCreated && bitmapCompressed && streamClosed);
+    }
+
+    private boolean saveBitmap(String fullPath, Image bitmap) {
+        if (fullPath == null || bitmap == null)
+            return false;
+
+        boolean fileCreated = false;
+        boolean bitmapCompressed = false;
+        boolean streamClosed = false;
+
+        File imageFile = new File(fullPath);
+
+        if (imageFile.exists())
+            if (!imageFile.delete())
+                return false;
+
+        try {
+            fileCreated = imageFile.createNewFile();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(imageFile);
+            bitmapCompressed = true;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            bitmapCompressed = false;
+
+        } finally {
+            if (out != null) {
+                try {
+                    out.flush();
+                    out.close();
+                    streamClosed = true;
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    streamClosed = false;
+                }
+            }
+        }
+
+        return (fileCreated && bitmapCompressed && streamClosed);
+    }
+
+
+    /**
+     * Get String value from SharedPreferences at 'key'. If key not found, return ""
+     * @param key SharedPreferences key
+     * @return String value at 'key' or "" (empty String) if key not found
+     */
+    public String getString(String key) {
+        return preferences.getString(key, "");
+    }
+
+
+
+    /**
+     * Check if external storage is writable or not
+     * @return true if writable, false otherwise
+     */
+    public static boolean isExternalStorageWritable() {
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
+    }
+
+    /**
+     * Check if external storage is readable or not
+     * @return true if readable, false otherwise
+     */
+    public static boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+
+        return Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state);
+    }
+
+}
--- app/src/main/java/com/example/cura/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/cura/ui/home/HomeFragment.java
@@ -1,35 +1,98 @@
 package com.example.cura.ui.home;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.cura.CameraActivity;
 import com.example.cura.R;
+import com.example.cura.UserMainActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
 
+    Button addFood, addIngredient, analyze, addIngredientUsingCamera;
+    EditText foodItem, foodIngredient;
+    TextView foodIngredientList;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
+        addFood = root.findViewById(R.id.saveFoodItem);
+        addIngredient = root.findViewById(R.id.addIngredientsButton);
+        analyze = root.findViewById(R.id.analyseButton);
+        foodItem = root.findViewById(R.id.foodName);
+        foodIngredient = root.findViewById(R.id.addIngredientsEditText);
+        foodIngredientList = root.findViewById(R.id.ingredientsListToShow);
+        addIngredientUsingCamera = root.findViewById(R.id.addIngredientsButtonUsingCamera);
+
+        addIngredientUsingCamera.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                if (checkAndRequestPermissions(getActivity()))
+                {
+                    startActivity(new Intent(getContext(), CameraActivity.class));
+                }
+            }
+        });
+
+        final ArrayList<String> stringArrayList = new ArrayList<>();
+        addIngredient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (foodIngredient.getText().toString().length()>1)
+                {
+                    String s = foodIngredient.getText().toString();
+                    stringArrayList.add(s);
+                }
             }
         });
         return root;
     }
+    public static final int REQUEST_ID_MULTIPLE_PERMISSIONS = 101;
+
+    public static boolean checkAndRequestPermissions(final Activity context) {
+        int ExtstorePermission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE);
+        int cameraPermission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.CAMERA);
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        if (cameraPermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded.add(Manifest.permission.CAMERA);
+        }
+        if (ExtstorePermission != PackageManager.PERMISSION_GRANTED) {
+            listPermissionsNeeded
+                    .add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(context, listPermissionsNeeded
+                            .toArray(new String[listPermissionsNeeded.size()]),
+                    REQUEST_ID_MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
+
 }
